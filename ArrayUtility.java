54b4cee6dfbe85aa8a670d271cf438ca5f29b940
Swarupa26 <91359055+Swarupa26@users.noreply.github.com>
2021-10-04 16:10:23 +0530

    Update ArrayUtility.java

--- ArrayUtility.java
+++ ArrayUtility.java
@@ -3,10 +3,10 @@ public class ArrayUtility
   public static int[] sortArray(int[] a)
   {
   }
-  public static int countElement(int[] a,element e)
+  public static int countElement(int[] a,int element)
   {
   }
-  public static int findElement(int[] a,element e)
+  public static int findElement(int[] a,int element)
   {
   }
 }
